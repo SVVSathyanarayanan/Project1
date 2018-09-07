80f914cbea68d33f6b6bb09301c84ff9886da28a
Manikandan Vaidyanathan <svvmanikandan@gmail.com>
2018-09-07 06:28:09 +0530

    Successfully Added

---
+++ Desktop/MotorVehicle/MotorVehicle/Login Page.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MotorVehicle
+{
+    public partial class Login_Page : Form
+    {
+        public Login_Page()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Motor_frm a = new Motor_frm();
+            a.Show();
+            this.Hide();
+        }
+        private void Login_Page_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Desktop/MotorVehicle/MotorVehicle/Search.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MotorVehicle
+{
+    public partial class Search : Form
+    {
+        public Search()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+       
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox1.TextLength == 3)
+            {
+                textBox2.Focus();
+            }
+            if(textBox1.Text!=)
+            {
+                MessageBox.Show("Error");
+            }
+            textBox1.Text = "";
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+              if (textBox2.TextLength == 3)
+                {
+                    textBox3.Focus();
+                }
+                         
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (textBox3.TextLength == 3)
+            {
+                Search_btn.Focus();
+            }
+        }
+    }
+}
